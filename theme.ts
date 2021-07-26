56fd47ff238a58b0c2068a9a644c0567d825813d
jasteralan <jaster@kiwinewenergy.com>
2021-07-26 19:27:53 +0800

    resume page

---
+++ theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+    fonts: {
+        openSans: 'Open Sans,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
+        montserrat: 'Montserrat,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"'
+    },
+})
+
+export default theme;
+  
