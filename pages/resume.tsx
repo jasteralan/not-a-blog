8cc3fa1dad8549847478eb4ab945d5e3fdfc66ff
jasteralan <jaster@kiwinewenergy.com>
2021-07-27 16:52:21 +0800

    add links

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import Head from "next/head";
-import { Box, Button, ButtonProps, Flex, Link, Text } from "@chakra-ui/react";
+import Link from 'next/link';
+import { Box, Button, ButtonProps, Flex, Link as ChakraLink, Text } from "@chakra-ui/react";
 import { AnimatePresence } from 'framer-motion';
+import { FiDownload } from "react-icons/fi";
 
 import { svgIcon } from "helpers/helpers";
 import ResumeEn from "components/resume/ResumeEn";
 import ResumeZhHant from "components/resume/ResumeZhHant";
 
 import s from "styles/resume.module.css";
-import { FiDownload } from "react-icons/fi";
 
 function DownloadBtn({ link, children, ...rest }: {
     link: string,
@@ -17,31 +18,33 @@ function DownloadBtn({ link, children, ...rest }: {
 }) {
     return (
         <Button variant="link" { ...rest }
-        ><Link href={ link } target="_blank"> 
+        ><ChakraLink href={ link } target="_blank"> 
             <Text as="span" pr={1}>{ children }</Text> 
             <FiDownload style={{ display:"inline", position:'relative', bottom: '2px' }} />
-        </Link></Button>
+        </ChakraLink></Button>
     )
 }
 export default function Resume() {
     const [lang, setLang] = useState('en');
-    const print = () => {
-        window?.print();
-        return false;
-    };
 
     return (
     <>
         <Head>
-            <title>Resume | 履歷</title>
+            <title>履歷 | Resume</title>
             <link rel="icon" href={svgIcon('💼')}></link>
         </Head>
         <Box w="full" minH="100vh" p={8} bg="gray.50" className={s.resumeWrapper}>
-            <Flex justify="flex-end" maxW="210mm" mx={"auto"} mb={4} className={s.resumeCta}> 
-                <DownloadBtn link={"/resumes/resume_en.pdf"} mr={4}>EN</DownloadBtn>
-                <DownloadBtn link={"/resumes/resume_zhHant.pdf"} mr={4}>繁</DownloadBtn>
+            <Flex justify="space-between" align="center" maxW="210mm" mx={"auto"} mb={4} className={s.resumeCta}> 
+                <Box>
+                    <Button variant="link" color="gray.400"><Link href="/">Home</Link></Button>
+                    <Text as="span" mx={4} color="gray.300">|</Text>
+                    <Button variant="link" color="gray.400"><Link href="/posts">Posts</Link></Button>
+                </Box>
 
                 <Box>
+                    <DownloadBtn link={"/resumes/resume_en.pdf"} mr={4}>EN</DownloadBtn>
+                    <DownloadBtn link={"/resumes/resume_zhHant.pdf"} mr={4}>繁</DownloadBtn>
+
                     <Button borderRightRadius="none" borderLeftRadius="full" pl={6}
                             isActive={lang === 'en'} colorScheme={lang === 'en'? "teal" : "gray"}
                             onClick={() => setLang('en')}
